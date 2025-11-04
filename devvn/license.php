6cbb33eace91a6e4d566d47658a386a59ca3dbb0
quynhvunb <quynhvu86@gmail.com>
2025-11-04 11:47:03 +0700

    devvn

---
+++ devvn/license.php
@@ -0,0 +1,15 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Bạn có thể cố sửa file này, nhưng nó sẽ được ghi đè khi có bản update mới
+ * Mua để ủng hộ và giúp tôi có những update và sản phẩm mới tốt hơn nữa nhé: https://levantoan.com/san-pham/
+ * Cảm ơn bạn đã tin tưởng và sử dụng sản phẩm của tôi!
+ */
+
+if ( ! defined( 'VNCHECKOUT_LICENSE' ) ) {
+    define( 'VNCHECKOUT_LICENSE', 'WxyJkadW9jdGFtaGFuZy52biIsInd3dy5kdW9jdGFtaGFuZy52biJd');
+}
+if ( ! defined( 'VNCHECKOUT_LICENSE_KEY' ) ) {
+    define( 'VNCHECKOUT_LICENSE_KEY', 'DEVVN-8151-fatTvq7LNgtU8qYMN63KU1E0');
+}
